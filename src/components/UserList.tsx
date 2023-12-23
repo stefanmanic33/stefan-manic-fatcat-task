7d7990c749b4c28c1c7177b4cda70f269a92a279
stefanmanic33 <stefanmanic.office@gmail.com>
2023-12-23 17:15:01 +0100

    Create User List

---
+++ src/components/UserList.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+}
+
+export const UserList = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const data: User[] = await response.json();
+        setUsers(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              <strong>ID:</strong> {user.id} <br />
+              <strong>Name:</strong> {user.name} <br />
+              <strong>Username:</strong> {user.username} <br />
+              <strong>Email:</strong> {user.email} <br />
+              <strong>Phone:</strong> {user.phone} <br />
+              <hr />
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
